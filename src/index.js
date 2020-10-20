84129a266e1d45694ac459197542147b4af40f99
Olga Kharkevich <olga.kharkevich@gmail.com>
2020-10-20 14:44:22 +0300

    refactor: rename vars, use forEach

--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
 
 let dg = ['zero','one','two','three','four', 'five','six','seven','eight','nine'];
-let tn = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
-let tw = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+let teen = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
+let ty = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
 
 
 module.exports = function toReadable (number) {
     let str = '';
-    if (number === 0) str = 'zero';
+    if (number === 0) {
+        str = 'zero';
+        return str
+    } 
     number = number.toString();
-    let x = number.length;
-    let n = number.split(''); 
-    let sk = 0;
-    for (let i = 0;   i < x;  i++) {
-        if ((x-i)%3==0) {
-            str += dg[n[i]] + ' ' + 'hundred ';
-        } else if ((x-i)%2==0) {
-            if (n[i]=='1') {
-               str += tn[n[i+1]] + ' ';
-               sk=1;
-            } else if ((sk==0)&&(n[i]!=0)) {
-                str += tw[n[i]-2] + ' '; 
-            } else if ((n[i]!=0)&&(sk==0)) {
-                str += dg[n[i]];
-            }
-        } else if ((n[i]==n[x-1])&&(n[i]!=0)&&(sk==0)) str += dg[n[i]];
-    }   
+    let len = number.length;
+    let numArr = number.split(''); 
+    let isTeen = false;
+
+    numArr.forEach((el, i) => {
+        if ((len - i) % 3 == 0) {
+            str += `${dg[el]} hundred `;
+        } else if ((len - i) % 2 == 0) {
+            if (el == '1') {
+                str += `${teen[numArr[i + 1]]} `;
+                isTeen = true;
+            } else if (!isTeen && (el != 0)) {
+                str += `${ty[el - 2]} `; 
+            } 
+        } else if ((el == numArr[len - 1]) && (el != 0) && !isTeen) {
+            str += dg[el];
+        } 
+    });  
     return str.trim();
 }
-
